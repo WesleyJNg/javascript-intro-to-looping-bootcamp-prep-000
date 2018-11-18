f656738bfd8c883e112f0502a8c222146ce7c754
Wesley Ng <wesjng+learnco@gmail.com>
2018-11-18 02:11:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,10 @@
 function forLoop(array) {
   for (var i = 1; i <26; i++) {
     if (i==1) {
-      array
+      array.push('I am 1 sstrange loop.');
+    }
+    else {
+      array.push(`I am ${i} strange loops.`);
     }
   }
 }
